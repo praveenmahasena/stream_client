5b7586ca6654205c3ca5b157f3554867d23b1414
praveenmahasena <praveenmahasena647@gmail.com>
2024-07-21 07:59:05 -0400

    idk what is happening

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,43 +1,55 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import axios from 'axios'
-import { onBeforeMount, ref } from 'vue'
+import { ref, onBeforeMount, reactive, onMounted } from 'vue'
+// @ ts-ignore
 
-const live = ref(false)
+const stream = ref(false)
 
 onBeforeMount(async () => {
-  try {
-    const { data } = await axios.get('http://localhost:42069/streamstatus', {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Token': localStorage.getItem('X-userName')
-      }
-    })
-    console.log(data)
-    live.value = data
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const { data } = await axios.get('http://localhost:42069/streamstatus', {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Token': localStorage.getItem('X-userName')
+            }
+        })
+        console.log(data)
+        stream.value = data
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 async function req() {
-  try {
-    const {data} = await axios.put('http://localhost:42069', null, {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Token': localStorage.getItem('X-userName')
-      }
-    })
-    live.value=data
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const { data } = await axios.put(
+            'http://localhost:42069/streamstatus',
+            { Status: stream.value },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Token': localStorage.getItem('X-userName')
+                }
+            }
+        )
+        stream.value = data
+    } catch (e) {
+        console.log(e)
+    }
 }
+
 </script>
 
 <template>
-  <button @click.prevent="req">
-    {{ !live ? 'stream' : 'no to stream' }}
-  </button>
+
+    <button @click.prevent="req">
+        {{ stream ? 'end stream' : 'start stream' }}
+    </button>
+
+
+
+
+
 </template>
 
 <style></style>
